af16099b4c6c63dbb7e68a3b213f60114ef579ea
yuryMitsiuk <yriymitsuik@gmail.com>
2018-04-21 17:25:58 +0300

    Completed initDB & populateDB scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,10 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100000, '2018-03-20 10:00:00', 'Завтрак', 500);
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100000, '2018-03-20 13:00:00', 'Обед', 1000);
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100000, '2018-03-20 20:00:00', 'Ужин', 500);
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100001, '2018-03-21 10:00:00', 'Завтрак', 1000);
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100001, '2018-03-21 13:00:00', 'Обед', 500);
+INSERT INTO meals (user_id, datetime, description, calories) VALUES (100001, '2018-03-21 20:00:00', 'Ужин', 510);
